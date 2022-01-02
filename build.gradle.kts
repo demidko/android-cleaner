699cc06a1b102c1466777fbf1df50471b48f2895
Daniil Demidko <demidko@farpost.com>
2022-01-02 17:17:11 +1000

    wip: windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ graal {
   mainClass("AppKt")
   outputName(projectDir.name)
   option("--no-fallback")
-  windowsVsVarsPath("C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\VC\\Auxiliary\\Build\\vcvars64.bat")
+  windowsVsVersion("2022")
 }
 tasks.build {
   dependsOn(tasks.nativeImage)
